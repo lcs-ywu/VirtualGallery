a1445aa539c7e1f994c4a47f908c7a225b8f84ec
James Wu <ywu@lcs.on.ca>
2021-03-04 12:41:25 -0500

    Get the carousel linked with artist lists and got it worked, going to add buttons in the view that lead to artist detail view

---
+++ Shared/Model/CarouselItem.swift
@@ -0,0 +1,8 @@
+//
+//  CarouselItem.swift
+//  VirtualGallery
+//
+//  Created by James Wu on 2021-03-04.
+//
+
+import Foundation
--- Shared/View/ArtistsList.swift
+++ Shared/View/ArtistsList.swift
@@ -6,19 +6,55 @@
 //
 
 import SwiftUI
+import ACarousel
 
 struct ArtistsList: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ContentView()
     }
 }
 
 struct ArtistsList_Previews: PreviewProvider {
     static var previews: some View {
-        ArtistsList()
+        ContentView()
     }
 }
 
+struct ContentView: View {
+    
+//    let artists: [Artist] = testArtistStore.artists { Artist.testArtist }
+    
+    
+    var body: some View {
+        
+        let store = ArtistsStore()
+        
+        ACarousel(store.artists,
+                  spacing: 10,
+                  headspace: 10,
+                  sidesScaling: 0.7,
+                  isWrap: true) { item in
+//                  ,autoScroll: .active(2)
+       
+            VStack{
+                Image(item.name)
+                    .resizable()
+                    .scaledToFill()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(height: 200)
+                    .cornerRadius(30)
+                Text(item.name).bold().italic().font(.title2)
+                
+                //Add a button
+                
+            }
+            
+            
+           
+        }
+        .frame(height: 300)
+    }
+}
 
 
 //Code for a carousel
