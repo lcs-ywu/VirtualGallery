1b1e3df52f780238abb835aa523b6a70e35ae6a2
Chen Sihan <schen@lcs.on.ca>
2021-05-31 14:22:04 -0400

    Added one information page

---
+++ Shared/View/InformationPage.swift
@@ -0,0 +1,20 @@
+//
+//  InformationPage.swift
+//  VirtualGallery
+//
+//  Created by Chen, Sihan on 2021-05-30.
+//
+
+import SwiftUI
+
+struct InformationPage: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct InformationPage_Previews: PreviewProvider {
+    static var previews: some View {
+        InformationPage()
+    }
+}
