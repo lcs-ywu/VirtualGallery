4a7b8efe532920ad66bbb375a0e61e81205d11e0
James Wu <ywu@lcs.on.ca>
2021-06-01 18:01:51 +0800

    Finish basic set up for contributor page

--- Shared/View/Contributors.swift
+++ Shared/View/Contributors.swift
@@ -9,7 +9,47 @@ import SwiftUI
 
 struct Contributors: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            HStack {
+                Text("About Us").bold().foregroundColor(.black).font(.system(.largeTitle, design: .serif)).padding(.all)
+                Spacer()
+            }.padding()
+            HStack {
+                VStack {
+                    Image("James Wu").resizable().scaledToFit().frame(width: 200, height: 200)
+                    Text("James Wu '21").font(.system(.title2, design: .serif))
+                }
+                VStack {
+                    Image("Sihan Chen").resizable().scaledToFit().frame(width: 200, height: 200)
+                    Text("Sihan Chen '22").font(.system(.title2, design: .serif))
+                }
+            }.padding()
+            
+            HStack {
+                Text("""
+     This a collaboration project created by James Wu and Sihan Chen, please let us how how we could do better:)
+
+     Spacial Thanks to Mr. Gordon, Ms. Morencie and every artist on campus who supported us.
+    """).font(.system(.title2, design: .serif)).padding()
+                    .multilineTextAlignment(.leading)
+                Spacer()
+            }
+            
+            HStack {
+                Text("Contact Us").bold().foregroundColor(.black).font(.system(.title, design: .serif)).padding(.all)
+                Spacer()
+            }.padding()
+            HStack {
+                Text("""
+    Please email us if you have any suggestions.
+    James: 2278147344@qq.com
+    Sihan: schen@lcs.on.ca
+    """).font(.system(.title2, design: .serif)).padding(.all)
+                    .multilineTextAlignment(.leading)
+                Spacer()
+            }.padding()
+            Spacer()
+        }
     }
 }
 
